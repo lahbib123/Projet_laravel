6fa9c548fb103eb4790f7736ad4e9f0e182c1c1a
lahbib <DrissRais@gamil.com>
2023-06-16 11:39:15 +0100

    init

---
+++ resources/views/client/components/molecules/about/address.blade.php
@@ -0,0 +1,14 @@
+<div class="address py-md-5 py-2" id="address">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-12">
+        <h3 class="font-primary"><u>Address</u></h3>
+      </div>
+      <div class="col-lg-8 col-md-8 col-12">
+        <div class="responsive-map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d222957.6505754417!2d-9.825481191981575!3d29.17311996854369!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0xdb453c81d25ac45%3A0x3373daa61d8bde58!2s46HC%2BWMH%2C%20P41%2C%20Guelmim!3m2!1d29.1297984!2d-9.7782622!5e0!3m2!1sfr!2sma!4v1686794464897!5m2!1sfr!2sma" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
